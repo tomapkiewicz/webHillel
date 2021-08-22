2e6adeec59ba156b8119f2280a20ad929e94ce62
Tom <Tom@SSD480>
2021-08-22 20:17:53 -0300

    Commit
    Former-commit-id: 77cabe9926a9e4d4d6eb8827d4677513960a2c46

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Link, Whatsapp
+from .models import Link, Whatsapp, MailContacto
 
 
 # Register your models here.
@@ -24,6 +24,17 @@ class WhatsappAdmin(admin.ModelAdmin):
         else:
             return ('name')
 
+class MailContactoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+    def get_readonly_fields(self, request, obj=None):
+     #   if request.user.groups.filter(name='admin').exists():
+        if request.user.is_superuser:
+            return ('created', 'updated')
+        else:
+            return ('mail')
+
 
 admin.site.register(Link, LinkAdmin)
 admin.site.register(Whatsapp, WhatsappAdmin)
+admin.site.register(MailContacto, MailContactoAdmin)
--- webhillel/settings.py
+++ webhillel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+k!@)$demq7!z@dlf4hk0jiy=z3l3_d)&cx4hxhtc^u85ps_rj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
