9f20e79db41f8f00a2b424af01388fa429e193d9
Tom <Tom@SSD480>
2021-09-01 23:45:15 -0300

    Commit

--- registration/admin.py
+++ registration/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import Profile,  FechaOnward, FechaTaglit
+from .models import Profile, FechaOnward, FechaTaglit
 from pages.models import Day, Category
 
 # Register your models here.
+
+
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','nombre','apellido','edad','whatsapp','provincia',)
+    list_display = ('user', 'nombre', 'apellido', 'edad', 'whatsapp', 'provincia',)
     readonly_fields = ('updated', 'created')
     search_fields = ('page__title', 'fecha')
+
 
 class FechaTaglitAdmin(admin.ModelAdmin):
     readonly_fields = ('updated', 'created')
@@ -15,12 +18,14 @@ class FechaTaglitAdmin(admin.ModelAdmin):
 class FechaOnwardAdmin(admin.ModelAdmin):
     readonly_fields = ('updated', 'created')
 
+
 class DayAdmin(admin.ModelAdmin):
     list_display = ('day',)
 
 
 class CategoriesAdmin(admin.ModelAdmin):
     readonly_fields = ('updated', 'created')
+
 
 admin.site.register(FechaTaglit, FechaTaglitAdmin)
 admin.site.register(FechaOnward, FechaOnwardAdmin)
