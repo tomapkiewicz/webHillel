b75ce5dcc7481070b2664978297c3352d28749cc
JeremiasFeferovich <95632444+JeremiasFeferovich@users.noreply.github.com>
2023-06-09 17:04:06 -0300

    added gzip compression
    Former-commit-id: e3cf22cb3f51a8e0cfa806c30e8eae7be3bbd3e8

--- webhillel/settings.py
+++ webhillel/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',  # gzip
 ]
 
 ROOT_URLCONF = 'webhillel.urls'
